98ea226945538e47d2be7807569d3290dfcd1235
Jeffrey Leung <jleung513@gmail.com>
2024-04-15 21:11:04 -0700

    Add Python script

---
+++ template.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+#
+# This Python 3 script...
+#
+# Style guide: https://peps.python.org/pep-0008
+
+### IMPORTS
+import os
+
+### FUNCTIONS
+
+
+### MAIN
+
+def main():
+    print('Hello World!')
+
+if __name__ == '__main__':
+    main()
+
